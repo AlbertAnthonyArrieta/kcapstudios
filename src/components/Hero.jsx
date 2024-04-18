2630049422ca8c571efc68e0adf22880cb6d34a0
Albert Arrieta <albert.arrieta@uleth.ca>
2024-04-18 14:22:38 -0600

    photography started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
-
 import NavButton from './NavButton.jsx';
+import video from '../assets/LandingPageVideo.mp4'; 
 
-import video from '../assets/LandingPageVideo.mp4'; // replace with your video file path
 
-
-export const Hero = ( {changePage} ) => {
+export const Hero = ({ changePage }) => {
 
 
 
     return (
         <div className="hero">
-            <h1 className="hero-title">WELCOME</h1>
-            {/* add autoPlay to play */}
-            <video loop muted className="hero-background">
-                <source src={video} type="video/mp4" />
-            </video>
-            <div className='navigation'>
-               <NavButton onClick={() => changePage('home')} label="home" />
-               <NavButton onClick={() => changePage('projects')} label="projects" />
-               <NavButton onClick={() => changePage('photography')} label="photography" />
-               <NavButton onClick={() => changePage('cinematography')} label="cinematography" />
-               <NavButton onClick={() => changePage('artwork')} label="artwork" />
+            <div></div>
+            <div></div>
+            <div className='hero-container'>
+                <div className='hero-background'>
+                    <div className='hero-header'>
+                        <div>LOGO</div>
+                        <h1 className="hero-title">WELCOME</h1>
+                        <a href="#about">contact me</a>
+                    </div>
+                    {/* add autoPlay to play */}
+                    <video loop muted className='hero-video'>
+                        <source src={video} type="video/mp4" />
+                    </video>
+                </div>
+                <div className='navigation'>
+                    <NavButton onClick={() => changePage('home')} label="home" />
+                    <NavButton onClick={() => changePage('projects')} label="projects" />
+                    <NavButton onClick={() => changePage('photography')} label="photography" />
+                    <NavButton onClick={() => changePage('cinematography')} label="cinematography" />
+                    <NavButton onClick={() => changePage('artwork')} label="artwork" />
+                </div>
             </div>
         </div>
     )
--- src/pages/Photography.jsx
+++ src/pages/Photography.jsx
@@ -1,9 +1,42 @@
+import RollingHeader from "../components/RollingHeader";
+
+import fenty1 from '../assets/fenty/1.png';
+import fenty2 from '../assets/fenty/8.jpg';
+import fenty3 from '../assets/fenty/17.jpg';
+import fenty4 from '../assets/fenty/23.jpg';
+import fenty5 from '../assets/fenty/37.jpg';
+import fenty6 from '../assets/fenty/18.jpg';
+import fenty7 from '../assets/fenty/4.jpg';
+import fenty8 from '../assets/fenty/7.jpg';
+
 export const Photography = () => {
     return (
         <>
-        <div className="photography page">
-            <h1>Photography</h1>
-        </div>
+            <div className="photography page">
+                <RollingHeader />
+                <div className="photography-project container">
+                    <div className="photography-details">
+                        <h1 className="photography-header">FENTY</h1>
+                        <p className="photography-p">Product photoshoot for Fenty Beauty Ambassador, Maxine Alba to endorse the brand’s “Gloss Bomb Universal Lip Luminizer” and “Plush Puddin’ Intensive Recovery Lip Mask.”</p>
+                    </div>
+                    <div className="gallery">
+                        <div className="gallery-double">
+                            <img src={fenty1} className="gallery-double-image"></img>
+                            <img src={fenty2} className="gallery-double-image"></img>
+                        </div>
+
+                        <div className="gallery-horizontal">
+                            <img src={fenty3} className="gallery-horizontal-image"></img>
+                        </div>
+
+                        <div className="gallery-double">
+                            <img src={fenty4} className="gallery-double-image"></img>
+                            <img src={fenty5} className="gallery-double-image"></img>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
